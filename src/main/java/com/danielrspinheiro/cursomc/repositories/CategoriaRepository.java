b1e8c8763954ae8a437355ebb992962c54cc6540
Daniel Pinheiro <nielpinheiro@gmail.com>
2019-02-20 16:32:35 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/danielrspinheiro/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.danielrspinheiro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danielrspinheiro.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
