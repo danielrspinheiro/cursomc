fa50718b5bb5a8a42a92026e443c9aef1bfdd5be
Daniel Pinheiro <nielpinheiro@gmail.com>
2019-02-26 14:28:04 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/danielrspinheiro/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.danielrspinheiro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danielrspinheiro.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
